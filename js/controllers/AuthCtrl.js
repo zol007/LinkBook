a5ece419698eeb49ad558899f567d526c6442120
Veronika Kasovska <v.kasovska@gmail.com>
2016-04-11 18:53:19 -0700

    move logout function to custom controller

--- js/controllers/AuthCtrl.js
+++ js/controllers/AuthCtrl.js
@@ -24,11 +24,4 @@ app.controller('authCtrl', function ($scope, $rootScope, $routeParams, $location
             }
         });
     };
-    $scope.logout = function () {
-        Data.get('logout').then(function (results) {
-            console.log(results);
-            Data.toast(results);
-            $location.path('login');
-        });
-    }
 });
